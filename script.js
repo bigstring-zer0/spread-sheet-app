c157f9f7634c065fd5f848e90c0894fe49e2e14a
bigstring-zer0 <147473025+bigstring-zer0@users.noreply.github.com>
2024-02-26 11:23:24 +0900

    initSpreadsheet() 메서드 구현

---
+++ script.js
@@ -0,0 +1,34 @@
+const spreadsheetContainer = document.querySelector("#spreadsheet-container")
+const ROWS = 10
+const COLS = 10
+const spreadsheet = []
+
+
+class Cell {
+    constructor(isHeader, disabled, data, row, column, active = false){
+        this.isHeader = isHeader
+        this.disabled = disabled
+        this.data = data
+        this.row = row
+        this.column = column
+        this.active = active
+    }
+}
+
+initSpreadsheet()
+
+function initSpreadsheet(){
+    for (let i = 0; i < ROWS; i++) {
+        let spreadsheetRow = []
+        for (let j = 0; j < COLS; j++) {
+            const cell = new Cell(false, false, i + "-" + j, i, j, i, j, false)
+            spreadsheetRow.push(cell)
+        }
+        spreadsheet.push(spreadsheetRow)
+    }
+    console.log("spreadsheet", spreadsheet)
+
+}
+
+
+
